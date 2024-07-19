e5fae4dd4d03b9ec17e487d3194a4bc10c15032b
zhdenny <dennyhz@gmail.com>
2024-07-19 16:52:47 -0400

    adapt to new which-key config

--- plugins/which-key.lua
+++ plugins/which-key.lua
@@ -7,7 +7,7 @@ return {
   end,
   config = function()
     local wk = require("which-key")
-    wk.register({
+    wk.add({
       -- ["<leader>f"] = { name = "+file" },
       -- ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find File" },
       -- ["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
@@ -16,12 +16,22 @@ return {
       -- ["<leader>gd"] = { "LSP-Definition" },
       -- ["<leader>gr"] = { "LSP-References" },
       -- ["<leader>ca"] = { "LSP-Code Action" },
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>i'] = { name = '[I]nfo', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+      -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+      -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+      -- ['<leader>i'] = { name = '[I]nfo', _ = 'which_key_ignore' },
+      -- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+      -- ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
       -- ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+      { "<leader>c", group = "[C]ode", icon = { icon = "", color = "yellow" } },
+      { "<leader>c_", hidden = true },
+      { "<leader>d", group = "[D]ocument", icon = { icon = "📄", color = "yellow" } },
+      { "<leader>d_", hidden = true },
+      { "<leader>i", group = "[I]nfo", icon = { icon = "", color = "yellow" } },
+      { "<leader>i_", hidden = true },
+      { "<leader>r", group = "[R]ename", icon = { icon = "", color = "yellow" } },
+      { "<leader>r_", hidden = true },
+      { "<leader>s", group = "[S]earch", icon = { icon = "", color = "yellow" } },
+      { "<leader>s_", hidden = true },
     })
   end,
 }
