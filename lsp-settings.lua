c5830904c4d1e2956e40bddb5c469b0091cc0f9c
zach <zach@ubuntuvm.localdomain>
2024-07-23 08:53:45 -0400

    lsp fix

--- lsp-settings.lua
+++ lsp-settings.lua
@@ -143,6 +143,7 @@ cmp.setup({
 		{ name = "path", max_item_count = 10 },
 	},
 })
+
 -- The nvim-cmp almost supports LSP's capabilities so you should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
