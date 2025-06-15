3397702cdfe6f73ae9fc3b5d0e278ade9b93da70
zhdenny <dennyhz@gmail.com>
2025-06-15 12:48:37 -0400

    telescope update

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -1,6 +1,9 @@
 return {
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -22,7 +22,7 @@ end
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.6",
+		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
