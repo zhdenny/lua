9fb2822b7c86c7c1580dd1c3de016a35809a3eff
zhdenny <dennyhz@gmail.com>
2025-09-29 16:30:29 -0400

    updated vim-mtux-navigator syntax

--- plugins/vim-tmux-navigator.lua
+++ plugins/vim-tmux-navigator.lua
@@ -1,7 +1,10 @@
 return {
   "christoomey/vim-tmux-navigator",
-  vim.keymap.set("n", "C-h", ":TmuxNavigateLeft<CR>"),
-  vim.keymap.set("n", "C-j", ":TmuxNavigateDown<CR>"),
-  vim.keymap.set("n", "C-k", ":TmuxNavigateUp<CR>"),
-  vim.keymap.set("n", "C-l", ":TmuxNavigateRight<CR>"),
+  lazy = false,
+  config = function()
+    vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>")
+    vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>")
+    vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>")
+    vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>")
+  end,
 }
