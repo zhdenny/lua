c55ca6874a57cd202e434b60c3a50e3dabb67ea5
zhdenny <dennyhz@gmail.com>
2025-09-28 21:35:16 -0400

    updates lualine and buffer hotkeys

--- plugins/lualine.lua
+++ plugins/lualine.lua
@@ -6,6 +6,40 @@ return {
       options = {
         theme = "catppuccin-frappe",
       },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = {
+          {
+            'buffers',
+            show_filename_only = true,
+            hide_filename_extension = false,
+            show_modified_status = true,
+            mode = 2, -- 0: buffer name, 1: buffer index, 2: buffer name + buffer index
+            max_length = vim.o.columns * 2 / 3,
+            filetype_names = {
+              TelescopePrompt = 'Telescope',
+              dashboard = 'Dashboard',
+              packer = 'Packer',
+              fzf = 'FZF',
+              alpha = 'Alpha'
+            },
+            use_mode_colors = false,
+            buffers_color = {
+              active = 'lualine_a_normal',
+              inactive = 'lualine_a_inactive',
+            },
+            symbols = {
+              modified = ' ●',
+              alternate_file = '#',
+              directory = '',
+            },
+          }
+        },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
     })
   end,
 }
--- vim-options.lua
+++ vim-options.lua
@@ -13,7 +13,8 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "No [H]ighlight search" })
+vim.keymap.set("n", "<leader>h", "<cmd>bp<CR>", { desc = "Buffer Previous" })
+vim.keymap.set("n", "<leader>l", "<cmd>bn<CR>", { desc = "Buffer Next" })
 
 -- Adjust window sizes
 vim.keymap.set("n", "<leader><Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
@@ -88,9 +89,9 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
