f47b5075222b5a3ec0c3d7464afc495b838f8483
zhdenny <dennyhz@gmail.com>
2025-10-05 10:08:49 -0400

    move popup messages to bottom right and clean up lspconfig

--- plugins/noice.lua
+++ plugins/noice.lua
@@ -87,6 +87,15 @@ return {
     })
     require("notify").setup({
       background_colour = "#000000",
+      top_down = false,
+      timeout = 3000, -- How long notifications stay visible (milliseconds)
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
     })
   end,
 }
+
