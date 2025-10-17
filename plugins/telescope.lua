a15b49eb64719bf9e730a9dd2f93929acf0cc5d8
zhdenny <dennyhz@gmail.com>
2025-10-17 12:19:51 -0400

    updates
    - Added difftastic
    - Added nerdy to quickly search nerd font icons with :Nerdy
    - Updated telescope
    - Updated which-key

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -111,8 +111,9 @@ return {
       vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
       vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
       vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" })
-      vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+      vim.keymap.set("n", "<leader>n", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" })
+      vim.keymap.set("n", "<leader>?", "<cmd>Cheatsheet<CR>", { desc = "Cheatsheet" })
+      vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Find existing buffers" })
     end,
   },
   {
